92c4da89ef06a3ddd1cbbb0bbadb254b829fef34
Gustavo Scharf <scharfgustavo@gmail.com>
2020-07-17 00:50:33 -0400

    first commit. old project. updating basically

---
+++ main.py
@@ -0,0 +1,66 @@
+from browser import getDict, handler, getWorkOrderTitles 
+from spreadsheet import createSpreadsheet, addTitles
+from sendMail import sendMail
+import time, datetime
+import pickle
+
+
+dateToday = datetime.date.today()
+
+sysPATH = "C:\\Users\\Gustavo\\Documents\\Programming Stuff\\check if ready\\bin\\new\\huh\\"
+picklePath1 = sysPATH+"pickleDumps\\workOrderTickets "+str(dateToday)+".p"
+picklePath2 = sysPATH+"pickleDumps\\workOrderResponses "+str(dateToday)+".p"
+picklePath3 = sysPATH+"pickleDumps\\workOrderTitles "+str(dateToday)+".p"
+excelPath = sysPATH+"excelDumps\\"+str(dateToday)+" "+time.strftime("%a %I%p.xlsx")
+
+# vv this is for testing
+
+# workOrderTickets = pickle.load(open(picklePath1,'rb'))
+
+
+# VV this is what usually should be turned on
+
+workOrderTickets = getDict()
+pickle.dump(workOrderTickets,open(picklePath1,'wb'))
+
+# print(workOrderTickets)
+
+workOrderResponses = {}
+for workOrder in workOrderTickets:
+	workOrderResponses[workOrder] = {}
+	for ticket in workOrderTickets[workOrder]:
+		workOrderResponses[workOrder][ticket] = []
+
+
+
+# vv this is for testing
+# workOrderResponses = pickle.load(open(picklePath2,'rb'))
+
+# VV this is what usually should be turned on
+
+workOrderResponses = handler(workOrderResponses)
+pickle.dump(workOrderResponses,open(picklePath2,'wb'))
+
+
+titleDict = getWorkOrderTitles(workOrderResponses)
+pickle.dump(titleDict, open(picklePath3, 'wb'))
+print(titleDict)
+
+# vv this is for testing
+# titleDict = pickle.load(open(picklePath3, 'rb'))
+
+
+
+
+
+
+fileNames = createSpreadsheet(workOrderResponses,excelPath,titleDict)
+for file in fileNames:
+	# excelPath = sysPATH+"excelDumps\\"+file
+	addTitles(titleDict,file)
+
+sendMail(fileNames)
+
+print('Email sent out succesfully...')
+
+
---
+++ runPython.bat
@@ -0,0 +1,2 @@
+"C:\Users\Shazia\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Shazia\Documents\programming stuff\program for gustavo\huh\main.py"
+pause
